81e611cfab5722e7f14e1f14629b9c606984c15b
srivanik44 <srivanik44@gmail.com>
2024-12-13 15:23:24 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -3,7 +3,6 @@ public class Addition
   public static void main(string args[])
   {
      int a=5,b=10,c;
-     c=a+b;
-     System.out.println("Addition of a and b is:"+c);
+     System.out.println("Addition of a and b is:"+(a+b));
  }
 }
